7ea9d2d81e6438552e0a03842eb7262a1dc0fedd
Black-Male <Gkaranja20@gmail.com>
2022-11-25 12:29:47 +0300

    Work Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+function saturdayFun(activity){
+    if (activity === "bathe my dog"){
+        return "This Saturday, I want to bathe my dog!"
+    }else{
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+ const mondayWork = function(location){
+    if (location === "work from home"){
+        return "This Monday, I will work from home."
+    }else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(init){
+    const text1 = "a hard worker"
+    if (init === "*"){
+        return function(){
+            return `You are ${init}a hard worker${init}!`
+        }
+    } else if (init === "||"){
+        return function(){
+            return `You are ${init}a dedicated programmer${init}!`
+        }
+    }
+}
+    
+    
+    
